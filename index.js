2b6c5c055c71d5c1af24c0a76f1860f6fa8d5550
Sohelrana85238960 <mdsohelrana85238960@gmail.com>
2024-01-30 01:12:17 +0600

    1/2 done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose");
-const port = 3000
+const port = 3001
 
 
 
@@ -16,7 +16,7 @@ const { getEshopData, getEshopSingleData } = require('./APi/EshopProducts/eshopC
 const { getBookData } = require('./APi/books/booksController');
 const { getCartData, postCartData } = require('./APi/cart/cartController');
 const { getTipsData } = require('./APi/tips/tipsController');
-
+const SSLCommerzPayment = require('sslcommerz-lts')
 
 
 
@@ -29,6 +29,16 @@ app.use(express.json())
 
 
 //********* Middleware's Ends Here *********//
+
+
+// payment method
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false //true for live, false for sandbox
+
+
+
 
 
 
@@ -53,6 +63,54 @@ mongoose.pluralize(null);
 
 
 //************   All APi's Starts   ************************//
+
+
+// payments method api 
+app.post("/order", async(req, res)=>{
+    const id = req.body.productId;
+    console.log(id)
+    const product = await getEshopSingleData(id)
+    console.log(product)
+    // const data = {
+    //     total_amount: 100,
+    //     currency: 'BDT',
+    //     tran_id: 'REF123', // use unique tran_id for each api call
+    //     success_url: 'http://localhost:3030/success',
+    //     fail_url: 'http://localhost:3030/fail',
+    //     cancel_url: 'http://localhost:3030/cancel',
+    //     ipn_url: 'http://localhost:3030/ipn',
+    //     shipping_method: 'Courier',
+    //     product_name: 'Computer.',
+    //     product_category: 'Electronic',
+    //     product_profile: 'general',
+    //     cus_name: 'Customer Name',
+    //     cus_email: 'customer@example.com',
+    //     cus_add1: 'Dhaka',
+    //     cus_add2: 'Dhaka',
+    //     cus_city: 'Dhaka',
+    //     cus_state: 'Dhaka',
+    //     cus_postcode: '1000',
+    //     cus_country: 'Bangladesh',
+    //     cus_phone: '01711111111',
+    //     cus_fax: '01711111111',
+    //     ship_name: 'Customer Name',
+    //     ship_add1: 'Dhaka',
+    //     ship_add2: 'Dhaka',
+    //     ship_city: 'Dhaka',
+    //     ship_state: 'Dhaka',
+    //     ship_postcode: 1000,
+    //     ship_country: 'Bangladesh',
+    // };
+    // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    // sslcz.init(data).then(apiResponse => {
+    //     // Redirect the user to payment gateway
+    //     let GatewayPageURL = apiResponse.GatewayPageURL
+    //     res.redirect(GatewayPageURL)
+    //     console.log('Redirecting to: ', GatewayPageURL)
+    // });
+    
+    
+})
 
 
 //article api's
