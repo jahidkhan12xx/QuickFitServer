be9fd9db3c3bcb1dc9580b58d255b8ee0cd10e94
Jahid Khan <jahidkhan12xx@gmail.com>
2024-02-29 16:15:38 +0600

    appaaas

--- APi/Favourite/favouriteController.js
+++ APi/Favourite/favouriteController.js
@@ -1,43 +1,37 @@
+const favCollection = require("../../Database/Schema/favourite/favourite.js");
 
-const favCollection = require("../../Database/Schema/favourite/favourite.js")
+const addFavourites = async (req, res) => {
+  const data = req.body;
 
-const addFavourites = async (req,res) => {
-    const data = req.body;
-   
-    const findData = await favCollection.find({ email: data.email });
- if (findData.length > 0) { 
-   const isExist = findData.some(item => item.blogId === data.blogId);
-   if (!isExist) {
-     const result = await favCollection.create(data);
-     res.send({ insertedId: result?._id });
-   }
-    else {
-     res.send( { message: "already exists in favorites." });
-   }
- } 
- 
- else {
- 
-   const result = await favCollection.create(data);
-   res.send({ insertedId: result?._id });
- }
-
+  const findData = await favCollection.find({ email: data.email });
+  if (findData.length > 0) {
+    const isExist = findData.some((item) => item.blogId === data.blogId);
+    if (!isExist) {
+      const result = await favCollection.create(data);
+      res.send({ insertedId: result?._id });
+    } else {
+      res.send({ message: "already exists  favorites." });
+    }
+  } else {
+    const result = await favCollection.create(data);
+    res.send({ insertedId: result?._id });
+  }
 };
 
-const getFavourites = async(req,res) =>{
-    const userEmail = req.params.email;
-    const result = await favCollection.find({email:userEmail});
-    res.send(result);
-}
+const getFavourites = async (req, res) => {
+  const userEmail = req.params.email;
+  const result = await favCollection.find({ email: userEmail });
+  res.send(result);
+};
 
-const deleteFavourites = async (req,res) =>{
-    const favId = req.params.id;
-    const result = await favCollection.findByIdAndDelete(favId);
-    res.send(result);
-}
+const deleteFavourites = async (req, res) => {
+  const favId = req.params.id;
+  const result = await favCollection.findByIdAndDelete(favId);
+  res.send(result);
+};
 
 module.exports = {
-    addFavourites,
-    getFavourites,
-    deleteFavourites
-}
+  addFavourites,
+  getFavourites,
+  deleteFavourites,
+};
