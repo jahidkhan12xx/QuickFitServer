7fd8eabb0292e4787fe03b10cb97259c661b6004
Sohelrana85238960 <mdsohelrana85238960@gmail.com>
2024-03-01 15:15:56 +0600

    created get api

--- Database/Schema/trainer/trainer.js
+++ Database/Schema/trainer/trainer.js
@@ -4,9 +4,11 @@ const trainerSchema = new mongoose.Schema({
    name:String,
    email:String,
    experience:String,
-    price:String,
-    days:String,
-     specialization:String,
+   price:String,
+   days:String,
+   specialization:String,
+   userName:String,
+   userEmail:String,
   })
 
 module.exports = mongoose.model( "trainers", trainerSchema,"trainers")
